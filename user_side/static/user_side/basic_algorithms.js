b1e4ef674c1d23ba86b9a07623396aff591dfbcd
Konrad Maciejczyk <conradmac97@gmail.com>
2021-12-28 23:35:25 +0100

    almost finished with searching backend

--- user_side/static/user_side/basic_algorithms.js
+++ user_side/static/user_side/basic_algorithms.js
@@ -1,3 +1,4 @@
+/* Konrad Maciejczyk, 2021 - 2022 */
 function toggleDivs(a, b, time_interval){
     var opacity = 0;
     var intervalID = 0; 
@@ -36,4 +37,77 @@ function toggleDivs(a, b, time_interval){
         }
     }
     fadeOut();
+}
+
+class CheckboxController{
+    constructor(ids){
+        this.ids = ids;
+        this.items = undefined;
+        this.items_amount = 0;
+        this.is_all_selected = true;
+
+        this.find_checkboxes();
+        this.select_all();
+    }
+
+    find_checkboxes(){
+        this.items = document.querySelectorAll('#item_type');
+        this.items_amount = this.items.length;
+
+        for(let i=0; i<this.items_amount - 1; i++){
+            this.items[i].addEventListener('click', (item) => {
+                this.update_checkbox();
+            })
+        }
+
+        this.items[this.items_amount - 1].addEventListener('click', (item)=>{
+            this.select_deselect();
+        })
+    }
+
+    select_all(){
+        this.items.forEach((item) =>{
+            item.checked = true;
+        })
+    }
+
+    deselect_all(){
+        this.items[0].checked = true;
+
+        for(let i = 1; i<this.items_amount; i++){
+            this.items[i].checked = false;
+        }
+    }
+
+    select_deselect(){
+        if(this.is_all_selected){
+            this.deselect_all();
+            this.is_all_selected = !this.is_all_selected;
+        }else{
+            this.select_all();
+            this.is_all_selected = !this.is_all_selected;
+        }
+    }
+
+    update_checkbox(){
+        let checked_counter = 0;
+
+        for(var i=0; i<this.items_amount - 1; i++){
+            if(this.items[i].checked === true ){
+                checked_counter++;
+            }
+        }
+        switch(checked_counter){
+            case 3:{
+                this.items[this.items_amount - 1].checked = true; break;
+            }
+            case 0:{
+                this.items[0].checked = true; break;
+            }
+            default:{
+                this.items[this.items_amount - 1].checked = false;
+            }
+            
+        }
+    }
 }
--- user_side/views.py
+++ user_side/views.py
@@ -2,22 +2,111 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 
 from user_side.forms import SearchForm
-from worker_side.models import Book
+from worker_side.models import Author, Book, Director, Movie
+
+def get_books(title, author):
+    try:     
+        results = None   
+        if author:
+            results = Author.objects.get(name__istartswith=author).book_set.all().filter(title__istartswith=title)
+        else:
+            results = Book.objects.filter(title__icontains=title)
+        book = {}
+        output = []
+        print("KSIAZKI:", results)
+
+        for result in results:
+            book['title'] = result.title
+            book['full_title'] = result.full_title
+            author = ", ".join([author.name for author in result.author.all()])
+            book['author'] = author if author else "author(s) unknown"
+            book['isbn'] = result.isbn if result.isbn else 'isbn not available'
+            book['id'] = result.id
+            book['publisher'] = result.publisher.name if result.publisher.name else "publisher unknown"
+            book['pub_year'] = result.pub_year if result.pub_year else "publication year unknown"
+            book['description'] = result.description
+            book['condition'] = result.condition.name
+            book['availability'] = result.availability.name
+            book['cover'] = result.cover.url
+
+            output.append(book)
+            book = {}
+    except:
+        output = {}
+
+    return output
+
+def get_movies(title, director):
+    try:
+        results = None
+        if director:
+                results = Director.objects.get(name__istartswith=director).movie_set.all().filter(title__istartswith=title)
+        else:
+            results = Movie.objects.filter(title__icontains=title)
+    
+
+
+        results = Movie.objects.filter(title__istartswith=title)
+        movie = {}
+        output = []
+        print("FILMY:", results)
+
+        for result in results:
+            movie['title'] = result.title
+            movie['full_title'] = result.full_title
+            director = ", ".join([director.name for director in result.director.all()])
+            movie['director'] = director if director else "director unknown"
+            screenwriter = ", ".join([screenwriter.name for screenwriter in result.screenwriter.all()])
+            movie['screenwriter'] = screenwriter if screenwriter else "screenwriter(s) unknown"
+            movie['id'] = result.id
+            movie['pub_year'] = result.pub_year if result.pub_year else "publication year unknown"
+            movie['description'] = result.description
+            movie['condition'] = result.condition.name
+            movie['availability'] = result.availability.name
+            movie['cover'] = result.cover.url
+
+            output.append(movie)
+            movie = {}
+     
+    except:
+        output = {}
+  
+    return output
 
 def home(request):
     return render(request, "user_side/home.html")
 
 def search(request):
-
     if 'title' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
+            number_of_results = 0
+            data = form.cleaned_data
+            print("CO PRZYSZŁO: ", data)
+            books = {}
+            movies = {}
+            sr = {}
+
+            if data['book']:
+                books = get_books(data['title'], data['author'])
+
+            if data['movie']:
+                movies = get_movies(data['title'], data['author'])
+
+           
+            number_of_results = len(books) + len(movies)
+            
             context = {
-                'title': f"Search results for {form.cleaned_data['title']} - Online Library Catalog",
-                'results': Book.objects.filter(title__contains=form.cleaned_data['title'])[0].author.all()[0].name
+                'number_of_results': number_of_results,
+                'fields':{'title': data['title'], 'author': data['author'], 'filters': "books, movies and sound recordings" },
+                'books': books,
+                'movies': movies
             }
 
-        return render(request, "user_side/search.html", context)
+        #print("DANE:", context)
+            return render(request, "user_side/search.html", context)
+        else:
+            return redirect('user_side-home')
     else:
         return redirect("user_side-home")
 
